06849c005b496155862ced09ab9d41254abf4bbb
mayarkhaled111 <mayarkhaledmoh123@gmail.com>
2024-08-26 23:35:09 +0300

    change path url

--- src/APIS/payment.js
+++ src/APIS/payment.js
@@ -3,9 +3,13 @@ import axios from "axios";
 let token = localStorage.getItem('userToken')
 
 // to extract ID from Token
-const payload = token.split('.')[1];
-const decodedPayload = JSON.parse(atob(payload));
-const userId = decodedPayload.id;
+let userId = null;
+if (token) {
+  // Extract ID from token
+  const payload = token.split('.')[1];
+  const decodedPayload = JSON.parse(atob(payload));
+  userId = decodedPayload.id;
+}
 
 export default function checkout({cardId,shippingAddress}){
     return axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cardId}?url=http://localhost:5173`,{shippingAddress},{headers:{token}})
@@ -21,19 +25,3 @@ export function getAllOrders(){
     return axios.get(`https://ecommerce.routemisr.com/api/v1/orders/user/${userId}`)
 }
 
-// export function getAllOrders(){
-//     if (token) {
-//         try {
-//             const payload = token.split('.')[1];
-//             const decodedPayload = JSON.parse(atob(payload));
-//             const userId = decodedPayload
-//             if (userId) {
-//                 return axios.get(`https://ecommerce.routemisr.com/api/v1/orders/user/${userId}`);
-//             } else {
-//                 return Promise.reject("User ID is not found in the token.");
-//             }
-//         } catch (error) {
-//             return Promise.reject("Failed to decode token: " + error.message);
-//         }
-//     }
-// }
